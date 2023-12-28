a15ce0fcb4cebde3a28493cac523067ff3f9e717
Your Name <you@example.com>
2023-12-28 21:33:09 +0800

    df

--- src/Components/HomeProduct.jsx
+++ src/Components/HomeProduct.jsx
@@ -6,30 +6,37 @@ function HomeProduct() {
    const { HandleAddCart, HandleViewDetails } = useContext(ThemeContext);
    const [AllItems, SetAllItems] = useState([]);
    const [selectedProduct, setSelectedProduct] = useState(null); // To store the selected product
-  const [NumberOfItem,SetNumberOfItem] = useState(5)
+   const [NumberOfItem, SetNumberOfItem] = useState(5);
 
    // Fetching data using useEffect
    useEffect(() => {
-      axios.get(`https://fakestoreapi.com/products?limit=${NumberOfItem}`).then((Data) => {
-         SetAllItems(Data.data);
-      });
+      axios
+         .get(`https://fakestoreapi.com/products?limit=${NumberOfItem}`)
+         .then((Data) => {
+            SetAllItems(Data.data);
+         });
    }, [NumberOfItem]);
 
    // Function to show product details in a modal
-   const showProductDetails = (id, title, image, description,category) => {
-      HandleViewDetails(id, title, image, description,category);
+   const showProductDetails = (id, title, image, description, category) => {
+      HandleViewDetails(id, title, image, description, category);
       const product = AllItems.find((item) => item.id === id);
       setSelectedProduct(product);
    };
-  const HanldeMoreItem = () =>{
-  SetNumberOfItem(NumberOfItem + 5)
-  console.log(AllItems.length)
-  }
- 
+   const HanldeMoreItem = () => {
+      SetNumberOfItem(NumberOfItem + 5);
+      console.log(AllItems.length);
+   };
+   const HanldePrevItem = () =>{
+      if(NumberOfItem  <=5){
+         }else{
+            SetNumberOfItem(NumberOfItem-5);
+            }
+   }
    // card component to display product details
    const ProductDetailsModal = ({ product }) => {
       return (
-         <div id="detailsCard" className='card col-lg-6'>
+         <div id='detailsCard' className='card col-lg-6'>
             <img
                style={{
                   objectFit: "cover",
@@ -44,7 +51,7 @@ function HomeProduct() {
                <h5 className='card-title text-center fs-4 fw-bold'>
                   {product.title}
                </h5>
-                <h6 className="text-center">{product.category}</h6>
+               <h6 className='text-center'>{product.category}</h6>
                <h5 className='text-success card-text mt-3'>
                   Price: ${product.price}
                </h5>
@@ -67,7 +74,10 @@ function HomeProduct() {
          <div className='row gap-3 justify-content-center mt-5'>
             {AllItems.map(({ title, price, image, id }) => {
                return (
-                  <div key={id} className='card bg-black text-light   col-lg-2 p-2'>
+                  <div
+                     key={id}
+                     className='card bg-black text-light   col-lg-2 p-2'
+                  >
                      <img
                         style={{
                            objectFit: "cover",
@@ -81,13 +91,13 @@ function HomeProduct() {
                         <h5 className='card-title'>{title}</h5>
                         <h5 className='fw-bolder text-success'>₱ {price}</h5>
                      </div>
-                    <button
+                     <button
                         onClick={() => HandleAddCart(AllItems, id)}
                         className='btn btn-info fw-bold text-light m-2 w-50 '
                      >
                         Add to Cart
                      </button>
-                 
+
                      <button
                         onClick={() => showProductDetails(id, title)}
                         className='btn_con btn btn-secondary'
@@ -100,8 +110,16 @@ function HomeProduct() {
          </div>
          {/* Render the modal when a product is selected */}
          {selectedProduct && <ProductDetailsModal product={selectedProduct} />}
-         <div className="container">
-            <h5 onClick={HanldeMoreItem} style={{cursor:"pointer"}} className="text-center text-light">See More Product</h5>
+         <div className='container flex-column text-info mt-3 p-2  d-flex justify-content-center align-items-center   '>
+           <div className="gap-5 d-flex ">
+           <button onClick={HanldePrevItem} className='btn btn-dark text-info'>
+               <i className='fa-solid fa-arrow-left'></i>
+            </button>
+            <button onClick={HanldeMoreItem} className='btn btn-dark text-info'>
+               <i className='fa-solid fa-arrow-right'></i>
+            </button>
+           </div>
+           <h5>Load More Product</h5>
          </div>
       </>
    );
